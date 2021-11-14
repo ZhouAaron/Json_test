d9d4d569fa0e78b6e27fcc8a627ed7d2c1adab4e
aaron_Zhou1 <zj971021@163.com>
2021-11-14 19:39:57 +0800

    Json 02 number

--- josn_01/josn_01/leptjson.hpp
+++ josn_01/josn_01/leptjson.hpp
@@ -12,17 +12,20 @@
 typedef enum {LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER,  LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT}lept_type;
 typedef struct
 {
+    double n;
     lept_type type;
 }lept_value;
 enum{
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,//json只含有空白
     LEPT_PARSE_INVALID_VALUE,//值不是null,false, true 三者之一
-    LEPT_PARSE_ROOT_NOT_SINGULAR//一个值之后，在空白之后还有其他字符
+    LEPT_PARSE_ROOT_NOT_SINGULAR,//一个值之后，在空白之后还有其他字符
+    LEPT_PARSE_NUMBER_TOO_BIG
 };
 //解析json
 int lept_parse(lept_value* v, const char* json);
 //访问结果的函数，获取其类型
 lept_type lept_get_type(const lept_value* v);
+double lept_get_number(const lept_value* v);
 
 #endif /* leptjson_hpp */
